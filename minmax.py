7c474361daa1d7fbb51a7fd9e2e2707594ea1a1c
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-10-28 23:36:12 +0530

    Add files via upload

---
+++ minmax.py
@@ -0,0 +1,31 @@
+# def minmax(arr):
+#     lenn = int(len(arr)-1)
+#     # lst1=[arr[0],arr[1],arr[2],arr[3]]
+#     # lst2=[arr[1],arr[2],arr[3],arr[4]]
+#     # lst3=[arr[2],arr[3],arr[4],arr[0]]
+#     # lst4=[arr[3],arr[4],arr[0],arr[1]]
+#     # lst5=[arr[4],arr[0],arr[1],arr[2]]
+#     print(lenn)
+#     for i in lenn:
+#         lst1 = [arr[i]]
+#     print(lst1)    
+#     # lst1 = sum(lst1)
+#     # lst2 = sum(lst2)
+#     # lst3 = sum(lst3)
+#     # lst4 = sum(lst4)
+#     # lst5 = sum(lst5)
+
+#     # mai = [lst1,lst2,lst3,lst4,lst5]
+#     # minn = min(mai)
+#     # maxx = max(mai)
+#     # print(maxx,minn)   
+
+
+
+# ar = [1,2,3,4,5]
+def minmax(ar):
+    
+
+ar =[1,2,3,4,5]
+
+minmax(ar)
