443bf838939cb41e7aae782f20b8a2e653b56be3
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-11-16 15:01:16 +0530

    Create pythonpractice.py

---
+++ pythonpractice.py
@@ -0,0 +1,30 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+meal1 = "spam" + "eggs" + "beans"
+meal2 = "spam\neggs\nbeans"
+meal3 = "spam, eggs, beans"
+meal4 = """spam
+eggs
+beans"""
+
+
+# print(meal1)
+# print(meal2)
+# print(meal3)
+# print(meal4)
+
+
+# first_name = "John"
+# last_name = "Cleese"
+# age = 78
+ 
+# print(first_name + last_name + age)
+
+a = 45
+b = 15
+c = 3
+ 
+print(a - b / c)
+
+days = "Mon, Tue, Wed, Thu, Fri, Sat, Sun"
+print(days[::5])
