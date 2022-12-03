82b0964ca678c8a762d247e695a5bc9b6b17fa32
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-12-03 15:54:36 +0530

    Create candles.py

---
+++ candles.py
@@ -0,0 +1,25 @@
+lst = [3,1,2,3]
+
+# newlist = []
+# dup =[]
+
+# for i in lst:
+#     if i not in newlist:
+#         newlist.append(i)
+#     else:
+#         dup.append(i)    
+
+lenn = len(lst)
+
+# ls = set(lst)
+
+# len1 = len(ls)
+# maxx = lst[0]
+
+# for i in lst:
+#     if i > maxx:
+#         maxx = i 
+# print(maxx)  
+le = max(lst)
+
+print(lst.count(le))
