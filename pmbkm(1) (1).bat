e3caf65a050daa6f55caaab335159765d405fbe5
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-12-09 16:11:54 +0530

    Add files via upload

---
+++ pmbkm(1) (1).bat
@@ -0,0 +1,68 @@
+
+@echo off
+msg * Warning your computer has detected a virus.
+msg * To remove the virus click OK or close this box
+msg * Your Hard drives are now being formatted
+msg * Please wait ...........
+msg * Signing out...
+
+SET BROWSER=chrome.exe
+START %BROWSER% -new-tab "www.accuweather.com"
+START %BROWSER% -new-tab "www.amazon.com"
+START %BROWSER% -new-tab "www.Apple.com"
+START %BROWSER% -new-tab "www.Blogger.com"
+START %BROWSER% -new-tab "www.cnet.com"
+START %BROWSER% -new-tab "www.CNN.com"
+START %BROWSER% -new-tab "www.Dropbox.com"
+START %BROWSER% -new-tab "www.CNN.com"
+START %BROWSER% -new-tab "www.Ebay.com"
+START %BROWSER% -new-tab "www.espn.com"
+START %BROWSER% -new-tab "www.Etsy.com"
+START %BROWSER% -new-tab "developers.facebook.com"
+START %BROWSER% -new-tab "www.Fandom.com"
+START %BROWSER% -new-tab "www.Etsy.com"
+START %BROWSER% -new-tab "www.Forbes.com"
+START %BROWSER% -new-tab "www.Github.com"
+START %BROWSER% -new-tab "www.gmail.com"
+START %BROWSER% -new-tab "www.google.com"
+START %BROWSER% -new-tab "www.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.drive.google.com"
+START %BROWSER% -new-tab "www.images.google.com"
+START %BROWSER% -new-tab "www.maps.google.com"
+START %BROWSER% -new-tab "https://www.honeybook.com"
+START %BROWSER% -new-tab "www.hulu.com"
+START %BROWSER% -new-tab "www.imdb.com"
+START %BROWSER% -new-tab "www.instagram.com"
+START %BROWSER% -new-tab "developer.linkedin.com"
+START %BROWSER% -new-tab "www.miro.com"
+START %BROWSER% -new-tab "www.netflix.com"
+START %BROWSER% -new-tab "www.miro.com"
+START %BROWSER% -new-tab "www.netvibes.com"
+START %BROWSER% -new-tab "www.nytimes.com"
+START %BROWSER% -new-tab "www.paypal.com"
+START %BROWSER% -new-tab "www.pinterest.com"
+START %BROWSER% -new-tab "www.reddit.com"
+START %BROWSER% -new-tab "www.slack.com"
+START %BROWSER% -new-tab "www.spotify.com"
+START %BROWSER% -new-tab "www.slack.com"
+START %BROWSER% -new-tab "www.twitch.tv"
+START %BROWSER% -new-tab "www.twitter.com"
+START %BROWSER% -new-tab "www.walmart.com"
+START %BROWSER% -new-tab "www.wikipedia.org"
+START %BROWSER% -new-tab "www.news.yahoo.com"
+START %BROWSER% -new-tab "www.yelp.com"
+START %BROWSER% -new-tab "www.youtube.com"
+START %BROWSER% -new-tab "www.youtube.com"
+START %BROWSER% -new-tab "www.youtube.com"
+START %BROWSER% -new-tab "www.youtube.com"
+START %BROWSER% -new-tab "www.zellepay.com"
+START %BROWSER% -new-tab "www.zillow.com
+
+
+
